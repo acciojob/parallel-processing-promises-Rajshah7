bde56462fc6420fb80cfbf62d5ec7f2c3af1ca9f
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-27 21:32:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
+
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -8,30 +9,40 @@ const images = [
   { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-function loadImage(url){
-	return new Promise((resolve,reject) => {
-		const img = createElement('img');
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
 
-		img.src = url;
+    const img = document.createElement("img");
 
-		img.onload = () => resolve(img);
+    img.src = url;
 
-		img.onerror = () => reject("Image failed to load");
-	})
+    img.onload = () => resolve(img);
+
+    img.onerror = () => reject("Image failed to load");
+
+  });
 }
 
-btn.addEventListener("click",() => {
-	const promises = images.map(() => loadImage(image.url));
+btn.addEventListener("click", () => {
 
-	promise.all(promises).then((result) => {
-		output.innerHTML = "";
+  const promises = images.map((image) => loadImage(image.url));
 
-		result.forEach(() => {
-			output.appendChild(img);
-		})
-		
-	}).catch(err){
-		output.appendChild(err);
-		console.log(err);
-	}
-})
+  Promise.all(promises)
+    .then((result) => {
+
+      output.innerHTML = "";
+
+      result.forEach((img) => {
+        output.appendChild(img);
+      });
+
+    })
+    .catch((err) => {
+
+      output.innerHTML = err;
+
+      console.log(err);
+
+    });
+
+});
