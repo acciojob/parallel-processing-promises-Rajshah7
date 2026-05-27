1464a2e513f2fdaed06bfd283cf1db4cf4cd755d
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-27 21:32:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function loadImage(url){
+	return new Promise((resolve,reject) => {
+		const img = createElement('img');
+
+		img.src = url;
+
+		img.onload = () => resolve(img);
+
+		img.onerror = () => reject("Image failed to load");
+	})
+}
+
+btn.addEventListener("click",() => {
+	const promises = images.map(() => loadImage(image.url));
+
+	promise.all(promises).then((result) => {
+		output.innerHTML = "";
+
+		result.forEach(() => {
+			output.appendChild(img);
+		})
+		
+	}).catch(err){
+		output.appendChild(err);
+		console.log(err);
+	}
+})
